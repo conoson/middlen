ac93bbb7c24699e9e31989cd2ca9906137a7ad25
conoson <kos982000@gmail.com>
2023-09-18 10:20:12 +0900

    Initial Commit

---
+++ middlen/middlenApp.swift
@@ -0,0 +1,17 @@
+//
+//  middlenApp.swift
+//  middlen
+//
+//  Created by Kwon on 2023/09/18.
+//
+
+import SwiftUI
+
+@main
+struct middlenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
